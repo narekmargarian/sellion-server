e265fb2e5e57a5f61a0c55818661cfd9dbde5160
Narek Margaryan <m.nareknersesovich@gmail.com>
2026-01-12 05:59:14 +0400

    update 3 html login dashbor list

--- src/main/java/com/sellion/sellionserver/controller/MainWebController.java
+++ src/main/java/com/sellion/sellionserver/controller/MainWebController.java
@@ -1,19 +1,38 @@
 package com.sellion.sellionserver.controller;
 
+import com.sellion.sellionserver.repository.ClientRepository;
+import com.sellion.sellionserver.repository.OrderRepository;
+import com.sellion.sellionserver.repository.ProductRepository;
+import com.sellion.sellionserver.repository.ReturnOrderRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainWebController {
 
-    @GetMapping("/admin")
-    public String showDashboard() {
-        return "dashboard"; // Откроет dashboard.html
+    private final ClientRepository clientRepository;
+    private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
+    private final ReturnOrderRepository returnOrderRepository; // Добавили возвраты
+
+    public MainWebController(ClientRepository clientRepository,
+                             ProductRepository productRepository,
+                             OrderRepository orderRepository,
+                             ReturnOrderRepository returnOrderRepository) {
+        this.clientRepository = clientRepository;
+        this.productRepository = productRepository;
+        this.orderRepository = orderRepository;
+        this.returnOrderRepository = returnOrderRepository;
     }
 
-    // Сделаем так, чтобы пустой адрес тоже вел в админку
-    @GetMapping("/")
-    public String index() {
-        return "redirect:/admin";
+    @GetMapping("/admin")
+    public String showDashboard(Model model) {
+        model.addAttribute("clients", clientRepository.findAll());
+        model.addAttribute("products", productRepository.findAll());
+        model.addAttribute("orders", orderRepository.findAll());
+        model.addAttribute("returns", returnOrderRepository.findAll());
+        return "dashboard";
     }
+
 }
