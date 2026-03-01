4f643ee9929e2db7f1220b197c241f5cc8292675
Narek Margaryan <m.nareknersesovich@gmail.com>
2026-03-01 17:27:52 +0400

    1++++

--- src/main/java/com/sellion/sellionserver/controller/CustomErrorController.java
+++ src/main/java/com/sellion/sellionserver/controller/CustomErrorController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+
 @Controller
 public class CustomErrorController implements ErrorController {
 
